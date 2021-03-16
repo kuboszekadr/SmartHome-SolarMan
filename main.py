15bfdc6e934e7a274066008bf3a161274950116a
Adrian Kuboszek <adrian.kuboszek@avanade.com>
2021-03-16 07:47:24 +0100

    Added support for argparse and raw data saving

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import os
 import argparse
+import json
 
 from datetime import datetime, timedelta
 from src import SolarMan
+
+
+def save_json(folder: str, data: json):
+    global timestamp
+    global date_start
+
+    date_format = '%Y_%m_%d_%H%M%S'
+
+    file_name = './data/{folder}/solarman_{date_start}_{timestamp}.json'.\
+        format(
+            folder=folder,
+            date_start=date_start.strftime(date_format),
+            timestamp=timestamp.strftime(date_format)
+        )
+
+    with open(
+            file_name,
+            mode='w',
+            encoding='UTF-8') as f:
+        json.dump(data, f)
+
 
 parser = argparse.ArgumentParser(
     description='Downloads last 24 hour energy \
@@ -14,10 +36,12 @@ parser.add_argument('--date_start',
                     help='Start date of extraction (optional)- default now')
 args = parser.parse_args()
 
+timestamp = datetime.now()
+
 # determine date start date
 # if left empty, use current timestamp
 date_start = args.date_start
-date_start = date_start if date_start else datetime.now()
+date_start = date_start if date_start else timestamp
 
 # calculate end date as last 24 hours
 date_end = date_start - timedelta(hours=24)
@@ -42,3 +66,6 @@ data = solarman.get_inverter_data(
     timestamp_start=date_end,
     timestamp_end=date_start
 )
+
+# dump data
+save_json('raw', data)
