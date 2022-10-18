7746d69dda258e51828ca1ac7aa46f8f828cb440
A. Kuboszek (c) <a.kuboszek@avanade.com>
2022-10-18 22:16:01 +0200

    Updates

--- src/streams.py
+++ src/streams.py
@@ -10,7 +10,7 @@ def stream_to_disk(data, root: str, data_date: date):
 
     folder_path = os.path.join(root, folder_name)
     file_path = os.path.join(folder_path, f'{file_name}.json')
-    
+
     os.makedirs(folder_path, exist_ok=True)
     with open(file_path, 'w') as f:
         json.dump(data, f)
